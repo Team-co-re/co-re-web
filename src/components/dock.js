d2831f29ae2b84d7cd3bfc0df76807f6d17c8cc7
Largopie <106071687+Largopie@users.noreply.github.com>
2023-05-18 16:32:08 +0900

    아이콘 마우스 hover시 text 출력 및 크기 작아지게 구현

--- src/components/dock.js
+++ src/components/dock.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -19,21 +19,78 @@ const SubContainer = styled.div`
     height: 100%;
 `;
 
-const Icon = styled.div`
+const IconDiv = styled.div`
+    display: flex;
     margin: 16px;
     width: 64px;
     height: 64px;
-    background-color: black;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const IconImg = styled.div`
+    width: 52px;
+    height: 52px;
+    background-color: red;
+    transition: all 0.2s ease-in-out;
+    :hover {
+        cursor: pointer;
+        background-color: purple;
+        width: 48px;
+        height: 48px;
+    };
+`;
+
+const IconText = styled.div`
+    transition: all 0.2s ease-in-out;
+    visibility: ${(props) => props.visibility};
+    width: 100%;
+    height: 25%;
+    background-color: red;
+    color: black;
+    text-align: center;
+    font-size: 4px;
+`;
+
+const Image = styled.div`
+    transition: all 0.2s ease-in-out;
+    width: 100%;
+    height: 75%;
+    background-color: green;
+    
 `;
 
 const Menu = () => {
+    const [isHovering, setIsHovering] = useState(false);
+
     return (
         <Container>
             <SubContainer>
-                <Icon />
-                <Icon />
-                <Icon />
-                <Icon />
+                <IconDiv>
+                    <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
+                        {isHovering ? <><Image /><IconText>123</IconText></> : null}
+                    </IconImg>
+                </IconDiv>
+                
+                <IconDiv>
+                    <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
+                        {isHovering ? <><Image /><IconText>123</IconText></> : null}
+                    </IconImg>
+                </IconDiv>
+
+                <IconDiv>
+                    <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
+                        {isHovering ? <><Image /><IconText>123</IconText></> : null}
+                    </IconImg>
+                </IconDiv>
+
+                <IconDiv>
+                    <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
+                        {isHovering ? <><Image /><IconText>123</IconText></> : null}
+                    </IconImg>
+                </IconDiv>
+
             </SubContainer>
         </Container>
     );
