4b882acfcd0b05788ceed7336c29782b47b88d2b
Largopie <106071687+Largopie@users.noreply.github.com>
2023-05-19 18:03:29 +0900

    Icon 컴포넌트 분리하여 css 각각 적용되도록 수정

--- src/components/dock.js
+++ src/components/dock.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Icon from './icon';
 
 const Container = styled.div`
     position: absolute;
@@ -19,78 +20,14 @@ const SubContainer = styled.div`
     height: 100%;
 `;
 
-const IconDiv = styled.div`
-    display: flex;
-    margin: 16px;
-    width: 64px;
-    height: 64px;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-`;
-
-const IconImg = styled.div`
-    width: 52px;
-    height: 52px;
-    background-color: red;
-    transition: all 0.2s ease-in-out;
-    :hover {
-        cursor: pointer;
-        background-color: purple;
-        width: 48px;
-        height: 48px;
-    };
-`;
-
-const IconText = styled.div`
-    transition: all 0.2s ease-in-out;
-    visibility: ${(props) => props.visibility};
-    width: 100%;
-    height: 25%;
-    background-color: red;
-    color: black;
-    text-align: center;
-    font-size: 4px;
-`;
-
-const Image = styled.div`
-    transition: all 0.2s ease-in-out;
-    width: 100%;
-    height: 75%;
-    background-color: green;
-    
-`;
-
 const Menu = () => {
-    const [isHovering, setIsHovering] = useState(false);
-
     return (
         <Container>
             <SubContainer>
-                <IconDiv>
-                    <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
-                        {isHovering ? <><Image /><IconText>123</IconText></> : null}
-                    </IconImg>
-                </IconDiv>
-                
-                <IconDiv>
-                    <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
-                        {isHovering ? <><Image /><IconText>123</IconText></> : null}
-                    </IconImg>
-                </IconDiv>
-
-                <IconDiv>
-                    <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
-                        {isHovering ? <><Image /><IconText>123</IconText></> : null}
-                    </IconImg>
-                </IconDiv>
-
-                <IconDiv>
-                    <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
-                        {isHovering ? <><Image /><IconText>123</IconText></> : null}
-                    </IconImg>
-                </IconDiv>
-
+                <Icon text="기능1"/>
+                <Icon text="기능2"/>
+                <Icon text="기능3"/>
+                <Icon text="기능4"/>
             </SubContainer>
         </Container>
     );
---
+++ src/components/icon.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const IconDiv = styled.div`
+    display: flex;
+    margin: 16px;
+    width: 64px;
+    height: 64px;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const IconImg = styled.div`
+    width: 52px;
+    height: 52px;
+    background-color: red;
+    transition: all 0.2s ease-in-out;
+    :hover {
+        cursor: pointer;
+        background-color: purple;
+        width: 48px;
+        height: 48px;
+    };
+`;
+
+const IconText = styled.div`
+    transition: all 0.2s ease-in-out;
+    visibility: ${(props) => props.visibility};
+    width: 100%;
+    height: 25%;
+    background-color: red;
+    color: black;
+    text-align: center;
+    font-size: 4px;
+`;
+
+const Image = styled.div`
+    transition: all 0.2s ease-in-out;
+    width: 100%;
+    height: 75%;
+    background-color: green;
+    
+`;
+
+const Icon = ({text}) => {
+    const [isHovering, setIsHovering] = useState(false);
+
+    return (
+        <IconDiv>
+            <IconImg onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
+                {isHovering ? <><Image /><IconText>{text}</IconText></> : null}
+            </IconImg>
+        </IconDiv>
+    );
+};
+
+export default Icon;
