fef20dc33f57c1db9427718823bfbb04356849a4
Largopie <106071687+Largopie@users.noreply.github.com>
2023-05-29 14:36:36 +0900

    commentPage 보내기 및 응답 기본 틀 구현

--- src/pages/comment/commentPage.js
+++ src/pages/comment/commentPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { commentModalFalse } from '../../redux/slices/commentModalSlice';
 import { returnBaseProcess } from '../../redux/slices/headerProcessSlice';
 import sendImg from '../../assets/imgs/send.png';
-import { setCommentText } from '../../redux/slices/commentSlice';
+import { setCommentText, setSendMessages } from '../../redux/slices/commentSlice';
 
 const CommentPageContainer = styled.div`
     width: 800px;
@@ -75,8 +75,22 @@ const SendImgDiv = styled.div`
     height: 28px;
 `;
 
+const ChatContainer = styled.div`
+    overflow: scroll;
+    width: 100%;
+    height: 500px;
+    
+`;
+
+const Send = styled.div`
+    background-color: ${(props) => props.isSend ? 'yellow' : 'green'};
+`;
+
 const Comment = () => {
     const dispatch = useDispatch();
+    const sendTextList = useSelector((state) => state.comment.sendMessages);
+    const commentText = useSelector((state) => state.comment.text);
+    const resTextList = '# "Hello World"를 출력합니다\n print(\'Hello World\')';
 
     const closeClickHandler = () => {
         dispatch(commentModalFalse());
@@ -86,19 +100,37 @@ const Comment = () => {
     const pressEnterHandler = (e) => {
         if (e.key === 'Enter') {
             if(!e.shiftKey) {
-                e.preventDefault();
-                console.log('Enter key pressed without Shift key');
+                e.preventDefault();                
+                dispatch(setSendMessages({
+                    text: e.target.value,
+                    date: String(new Date()),
+                    isSend: true
+                }));
+                setTimeout(() => {
+                    dispatch(setSendMessages({
+                        text: resTextList,
+                        date: String(new Date()),
+                        isSend: false
+                    }));
+                    dispatch(setCommentText(''));
+                }, 2000);
+                
             };
         };
     };
+
+    console.log(sendTextList);
 
     return (
         <CommentPageContainer>
             <SubContainer>
                 <CloseBtn onClick={closeClickHandler}>X</CloseBtn>
+                <ChatContainer>
+                    {sendTextList.map(({text, date, isSend}, idx) => <Send isSend={isSend} key={text+date+idx}><textarea readOnly value={`${text} \n\ndate: ${date}`} /></Send>)}
+                </ChatContainer>
                 <ChatDiv>
                     <ChatSubDiv>
-                        <ChatInput onChange={(e) => dispatch(setCommentText(e.target.value))} onKeyDown={pressEnterHandler} />
+                        <ChatInput value={commentText} onChange={(e) => dispatch(setCommentText(e.target.value))} onKeyDown={pressEnterHandler} />
                         <SendBtn>
                             <SendImgDiv />
                         </SendBtn>
--- src/redux/slices/commentSlice.js
+++ src/redux/slices/commentSlice.js
@@ -3,18 +3,22 @@ import { createSlice } from '@reduxjs/toolkit';
 const commentSlice = createSlice({
     name: 'comment',
     initialState: {
-        messages: [],
+        sendMessages: [],
         text: '',
     },
     reducers: {
         setCommentText: (state, action) => {
             state.text = action.payload;
         },
+        setSendMessages: (state, action) => {
+            state.sendMessages.push(action.payload);
+        },
     },
 });
 
 export const {
     setCommentText,
+    setSendMessages,
 } = commentSlice.actions;
 
 export default commentSlice.reducer;
